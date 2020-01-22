294a434f4ab13cf6f9f730a13aed71ec51a834fa
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-01-22 09:43:44 +0700

    menambahkan helper connection

---
+++ SampleServerControl/Helpers/DBHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace SampleServerControl.Helpers
+{
+    public class DBHelper
+    {
+        public static string GetConn()
+        {
+            return ConfigurationManager
+                .ConnectionStrings["MyConnectionString"].ConnectionString;
+        }
+    }
+}
