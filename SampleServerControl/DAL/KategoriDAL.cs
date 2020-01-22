f979c0a08694ccea5f4c31bc0eb420417420456d
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-01-22 10:02:09 +0700

    menambahkan ado.net

--- SampleServerControl/DAL/KategoriDAL.cs
+++ SampleServerControl/DAL/KategoriDAL.cs
@@ -4,11 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
 using System.Configuration;
+using SampleServerControl.Models;
 
 namespace SampleServerControl.DAL
 {
     public class KategoriDAL
     {
-        
+        public IEnumerable<Kategori> GetAll()
+        {
+            using(SqlConnection conn = new SqlConnection(Helpers.DBHelper.GetConn()))
+            {
+                List<Kategori> lstKategori = new List<Kategori>();
+
+                string strSql = @"select * from Kategori 
+                                  order by nama_kat asc";
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        lstKategori.Add(
+                            new Kategori
+                            {
+                                id_kat = Convert.ToInt32(dr["id_kat"]),
+                                nama_kat = dr["nama_kat"].ToString()
+                            });
+                    }    
+                }
+                dr.Close();
+                cmd.Dispose();
+                conn.Close();
+
+                return lstKategori;
+            }
+        }
     }
 }
---
+++ SampleServerControl/Models/Kategori.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleServerControl.Models
+{
+    public class Kategori
+    {
+        public int id_kat { get; set; }
+        public string nama_kat { get; set; }
+    }
+}
