d4ca07a1f93292dd31599f40afed7105866bb488
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-01-22 11:38:44 +0700

    menambahkan interface

---
+++ SampleServerControl/DAL/ICrud.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleServerControl.DAL
+{
+    public interface ICrud<T>
+    {
+        IEnumerable<T> GetAll();
+        T GetByID(string id);
+        void Insert(T obj);
+        void Update(T obj);
+        void Delete(string id);
+    }
+}
---
+++ SampleServerControl/Models/Berita.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleServerControl.Models
+{
+    public class Berita
+    {
+        public int id_berita { get; set; }
+        public int id_kat { get; set; }
+        public string judul_berita { get; set; }
+        public string detail_berita { get; set; }
+        public DateTime tanggal { get; set; }
+        public bool isapprove { get; set; }
+        public string pics { get; set; }
+    }
+}
